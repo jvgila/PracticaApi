dfc35bd17677a5b201ecad0b43c465b3204bc6ed
jvgila <javier.velarde.gila@gmail.com>
2022-04-24 19:39:19 +0200

    Add files via upload

---
+++ ApiJvg.java
@@ -0,0 +1,21 @@
+package com.govertispractica.pr.api.entitys;
+
+public class ApiJvg {
+	
+	
+	private Long id;
+	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
