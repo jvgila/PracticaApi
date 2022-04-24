f092c766312dd05d23c8ba315d43f395fb04b143
jvgila <javier.velarde.gila@gmail.com>
2022-04-24 19:40:10 +0200

    Add files via upload

---
+++ PracticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govertispractica.pr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
